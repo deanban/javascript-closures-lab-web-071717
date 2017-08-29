914daf21fc4d89d23ed006ca74569587c7a1e962
deanban <pbp2005@gmail.com>
2017-08-29 09:44:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const app = "I don't do much."
+function bumpCounter(){
+	let counter = 0;
+	function addBump(){counter += 1};
+	function getBumps(){return counter};
+	return {addBump, getBumps}
+}
+
+function createAnimal(animalType){
+	return function (deadly_device){
+		return {animalType: animalType, deadlyDevice: deadly_device}
+	}
+}
+
+let sharkCreator = createAnimal('Shark');
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+let sharkWithFrickinCannon = sharkCreator('Cannon');
+
+
+
